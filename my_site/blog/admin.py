d15c618fa633dca0595fbdc454ca61708833e4ec
Bianca Velázquez <bianca.vlzr@gmail.com>
2025-06-13 16:12:06 +0200

    filters and displays columns in Admin Post panel

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models import Post, Tag, Author
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+  list_filter = ("author", "tags", "date",)
+  list_display = ("title", "date", "author",)
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Tag)
 admin.site.register(Author)
 
