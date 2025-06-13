95658f932d7f4cb43dea5fb5795440665a4c6707
Bianca Velázquez <bianca.vlzr@gmail.com>
2025-06-13 16:49:00 +0200

    Modifies views to get Posts from the DB

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,88 +1,16 @@
-from django.shortcuts import render
-from datetime import date
+from django.shortcuts import render, get_object_or_404
 
-
-all_posts = [
-  {
-    "slug": "hike-in-the-mountains",
-    "image": "mountains.jpg",
-    "author": "Someone",
-    "date": date(2025, 7, 12),
-    "title": "Mountain hiking",
-    "excerpt": "Amazing view",
-    "content": """"
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos atque quae cumque vitae
-        exercitationem esse, minima error, nisi odio, minus laboriosam aut. Ducimus nisi, pariatur inventore
-        necessitatibus sint rerum?
-
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos atque quae cumque vitae
-        exercitationem esse, minima error, nisi odio, minus laboriosam aut. Ducimus nisi, pariatur inventore
-        necessitatibus sint rerum?
-
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos atque quae cumque vitae
-        exercitationem esse, minima error, nisi odio, minus laboriosam aut. Ducimus nisi, pariatur inventore
-        necessitatibus sint rerum?
-    """
-
-  },
-  {
-    "slug": "hike-in-the-mountains",
-    "image": "mountains.jpg",
-    "author": "Someone",
-    "date": date(2025, 6, 12),
-    "title": "Mountain hiking",
-    "excerpt": "Amazing view",
-    "content": """"
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos atque quae cumque vitae
-        exercitationem esse, minima error, nisi odio, minus laboriosam aut. Ducimus nisi, pariatur inventore
-        necessitatibus sint rerum?
-
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos atque quae cumque vitae
-        exercitationem esse, minima error, nisi odio, minus laboriosam aut. Ducimus nisi, pariatur inventore
-        necessitatibus sint rerum?
-
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos atque quae cumque vitae
-        exercitationem esse, minima error, nisi odio, minus laboriosam aut. Ducimus nisi, pariatur inventore
-        necessitatibus sint rerum?
-    """
-
-  },
-  {
-    "slug": "hike-in-the-mountains",
-    "image": "mountains.jpg",
-    "author": "Someone",
-    "date": date(2025, 5, 12),
-    "title": "Mountain hiking",
-    "excerpt": "Amazing view",
-    "content": """"
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos atque quae cumque vitae
-        exercitationem esse, minima error, nisi odio, minus laboriosam aut. Ducimus nisi, pariatur inventore
-        necessitatibus sint rerum?
-
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos atque quae cumque vitae
-        exercitationem esse, minima error, nisi odio, minus laboriosam aut. Ducimus nisi, pariatur inventore
-        necessitatibus sint rerum?
-
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos atque quae cumque vitae
-        exercitationem esse, minima error, nisi odio, minus laboriosam aut. Ducimus nisi, pariatur inventore
-        necessitatibus sint rerum?
-    """
-
-  }
-]
-
-def get_date(post):
-  return post['date']
+from .models import Post
 
 # Create your views here.
 def starting_page(request):
-  sorted_posts = sorted(all_posts, key=get_date)
-  latest_posts = sorted_posts[-3:]
+  latest_posts = Post.objects.all().order_by("-date")[:3]
   return render(request, "blog/index.html", { "posts": latest_posts })
 
 def posts(request):
+  all_posts = Post.objects.all().order_by("-date")
   return render(request, "blog/all-posts.html", { "all_posts": all_posts })
 
 def post_details(request, slug):
-  identified_post = next(post for post in all_posts if post["slug"] == slug)
+  identified_post = get_object_or_404(Post, slug=slug)
   return render(request, "blog/post-detail.html", { "post": identified_post })
