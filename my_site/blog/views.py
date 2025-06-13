e747f037e47cd667bd605249b82eb141a0cc1218
Bianca Velázquez <bianca.vlzr@gmail.com>
2025-06-13 11:21:00 +0200

    adds dummy data to post details page

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -84,4 +84,5 @@ def posts(request):
   return render(request, "blog/all-posts.html", { "all_posts": all_posts })
 
 def post_details(request, slug):
-  return render(request, "blog/post-detail.html")
+  identified_post = next(post for post in all_posts if post["slug"] == slug)
+  return render(request, "blog/post-detail.html", { "post": identified_post })
