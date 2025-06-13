13ffe1b9eabc5ac370945ba09cc6bdbad8667ec7
Bianca Velázquez <bianca.vlzr@gmail.com>
2025-06-13 16:13:54 +0200

    adds pre-populated field (slug) in post admin panel

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -4,6 +4,7 @@ from .models import Post, Tag, Author
 class PostAdmin(admin.ModelAdmin):
   list_filter = ("author", "tags", "date",)
   list_display = ("title", "date", "author",)
+  prepopulated_fields = {"slug": ("title",)}
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Tag)
